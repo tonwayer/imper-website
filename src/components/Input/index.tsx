a187286a5b903446e1b4f79d3f41b77df65c97a4
mastercodercat <smartgeek.taylor@gmail.com>
2022-05-11 20:23:21 -0400

    Added Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,39 @@
+type Props = {
+  name: string,
+  label: string,
+  value: string,
+  error: Boolean,
+  errorMsg?: string,
+  handleChange: (e: React.ChangeEvent<any>) => void,
+}
+
+const Input = (props: Props) => {
+  return <>
+    <div>
+      <label className="mb-2 text-sm font-medium text-[#E5E7EB]">
+        {props.label}
+      </label>
+      <input
+        type="text"
+        id={props.name}
+        className={
+          "bg-[#65474D]  text-sm block p-2.5 rounded-lg text-white border "
+          + (
+            props.error
+              ? "border-error focus:ring-error"
+              : "border-white focus:ring-white"
+          )
+        }
+        value={props.value}
+        onChange={props.handleChange}
+      />
+      {
+        props.error && <p className="mt-2 text-sm text-error">
+          {props.errorMsg ? props.errorMsg : "Error Message"}
+        </p>
+      }
+    </div>
+  </>
+}
+
+export default Input
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,6 @@
-import React from "react"
-
 const Home = () => {
-  return <div>
 
+  return <div>
   </div>
 }
 
