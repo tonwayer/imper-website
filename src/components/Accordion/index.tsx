162bcebf76343e159e1a69dc77eeb15555a325a0
mastercodercat <smartgeek.taylor@gmail.com>
2022-06-09 08:12:26 -0400

    Fixed cursors

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -10,7 +10,7 @@ const Accordion = (props: Props) => {
 
   return (
     <div className="bg-[#65474D]/50 rounded-[8px]">
-      <div className="flex justify-between items-center p-4 w-full font-medium text-left text-white" onClick={() => setIsActive(!isActive)}>
+      <div className="flex justify-between items-center p-4 w-full font-medium text-left text-white cursor-pointer" onClick={() => setIsActive(!isActive)}>
         <div className="text-base font-bold">{props.title}</div>
         <div className="text-3xl">{isActive ? '-' : '+'}</div>
       </div>
--- src/components/CircleIcon/index.tsx
+++ src/components/CircleIcon/index.tsx
@@ -1,7 +1,7 @@
 import { Icon } from '@iconify/react';
 
 const CircleIcon = (props: { icon: string, fontSize?: string, classNames?: string }) => (
-  <div className={"rounded-full bg-[#65474D] w-12 h-12 flex items-center justify-center " + props.classNames}>
+  <div className={"rounded-full bg-[#65474D] w-12 h-12 flex items-center justify-center cursor-pointer " + props.classNames}>
     <Icon icon={props.icon} style={{ fontSize: props.fontSize || "24px" }} />
   </div>)
 
